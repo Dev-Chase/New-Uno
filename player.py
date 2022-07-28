af042e413adf3ed07f0bfb6ce170e9cd69394443
Dev-Chase <chasecurwin@gmail.com>
2022-07-28 14:10:29 -0400

    Added Base

---
+++ player.py
@@ -0,0 +1,7 @@
+from settings import *
+
+class Player():
+    # Creating Constructor for the Player Class
+    def __init__(self):
+        # Creating Sprite Group Attributes
+        self.cards = pygame.sprite.Group()
---
+++ settings.py
@@ -0,0 +1,7 @@
+from colours import colours
+import pygame
+
+W = 500
+H = 500
+
+# Global Data goes here
